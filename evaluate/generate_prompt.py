891ad4927f623475a7a95d33aa5d9da2a2f9183a
notbulubula <urbaniak.bruno@gmail.com>
2024-06-13 10:54:28 +0200

    Improving generation and generation test

--- evaluate/generate_prompt.py
+++ evaluate/generate_prompt.py
@@ -9,16 +9,11 @@ def generate_prompt(text, prompt_max_len, RAW_TEXT = False):
     if RAW_TEXT:
         return raw_text
 
-    # prompt = [
-    #     {"role": "system", "content": "Your are a wrighter of a story."},
-    #     {"role": "system", "content": "Your story strats as folows: " + output},
-    #     {"role": "user", "content": "Write the following part of your story and try to add new characters"}
-    # ]
 
     prompt = [
         {"role": "system", "content": "You are a skilled writer crafting an engaging story."},
         {"role": "assistant", "content": "The story starts as follows: " + raw_text},
-        {"role": "user", "content": "Continue the story from where it left off. The story should include previous characters but also introduce new characters."}
+        {"role": "user", "content": "Continue the story from where it left off. The story should include previous characters but also introduce few new characters."}
     ]
 
     return prompt
--- tests/test_generate_prompt.py
+++ tests/test_generate_prompt.py
@@ -9,9 +9,17 @@ class TestGeneratePrompt(unittest.TestCase):
     text = "Hye was waiting for her usual bus at the usual bus stop near her work place . It was late at night , almost 10pm . She was all alone . But , what she didn 't realize was that she wasn 't exactly all alone ."
     prompt_max_len = 200
 
-    def test_generate_prompt(self) -> None:
+    def test_generate_prompt_raw(self) -> None:
         raw_text = generate_prompt(self.text, self.prompt_max_len, RAW_TEXT = True)
         self.assertEqual(raw_text, "Hye was waiting for her usual bus at the usual bus stop near her work place . It was late at night , almost 10pm . She was all alone .")
 
+    def test_generate_prompt(self) -> None:
+        prompt = generate_prompt(self.text, self.prompt_max_len)
+        self.assertEqual(prompt, [
+            {"role": "system", "content": "You are a skilled writer crafting an engaging story."},
+            {"role": "assistant", "content": "The story starts as follows: Hye was waiting for her usual bus at the usual bus stop near her work place . It was late at night , almost 10pm . She was all alone ."},
+            {"role": "user", "content": "Continue the story from where it left off. The story should include previous characters but also introduce few new characters."}
+        ])
+        
 if __name__ == '__main__':
     unittest.main()
